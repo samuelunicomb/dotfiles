5349650618ecff8bf195cec6b6735ace4e3228e5
Samuel Unicomb <samuelunicomb@gmail.com>
2019-08-19 09:01:22 +0200

    tidying up byobu_lab comments

--- .byobu_lab.sh
+++ .byobu_lab.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-SESH="lab"
-# this doesn't work in quotes ??????????
-DIR=~/lab/redesign
-CURDIR=$(pwd)
+# simple script for opening a new byobu session in a desired directory layout
 
-echo $CURDIR
+# note : paths can't be defined in quotes ??????????
+
+SESH="lab"
+DIR1=~/lab/redesign
+DIR2=~/lab/redesign/src
 
 # new session for basic development of lab
-byobu new-session -d -s $SESH -c $DIR
+byobu new-session -d -s $SESH -c $DIR1
 
 # basic dev windows
-for i in {1..5}; do byobu new-window -t $SESH -n "" -c $DIR; done
+for i in {1..4}; do byobu new-window -t $SESH -n "" -c $DIR2; done
 
 # server windows
-for i in {6..7}; do byobu new-window -t $SESH -n "psmn" -c $DIR; done
+for i in {6..7}; do byobu new-window -t $SESH -n "psmn" -c $DIR1; done
