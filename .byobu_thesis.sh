97d3cea7b14e5191fa7351b6c054e2941bf7293c
Samuel Unicomb <samuelunicomb@gmail.com>
2019-08-21 11:13:02 +0200

    new script for thesis project layout in byobu

---
+++ .byobu_thesis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# simple script for opening a new byobu session in a desired directory layout
+
+# note : paths can't be defined in quotes??? eg DIR1="~/lab/redesign" not found
+
+SESH="thesis"
+DIR1=~/thesis
+DIR2=~/thesis/Chapters
+
+# new session for basic development of lab
+byobu new-session -d -s $SESH -c $DIR1
+
+# basic dev windows
+for i in {1..6}; do byobu new-window -t $SESH -n "Ch${i}" -c $DIR2; done
--- copydotfiles.sh
+++ copydotfiles.sh
@@ -5,5 +5,6 @@ DIR=~/dotfiles
 cp ~/.bashrc $DIR
 cp ~/.vimrc $DIR
 cp ~/.byobu_lab.sh $DIR
+cp ~/.byobu_thesis.sh $DIR
 cp ~/.tmux.conf $DIR
 
