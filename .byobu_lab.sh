c3229e71ee53c96f0a405665b33803434bf36e6c
Samuel Unicomb <samuelunicomb@gmail.com>
2019-11-23 14:47:40 +0100

    update byobu launch dirs

--- .byobu_lab.sh
+++ .byobu_lab.sh
@@ -5,14 +5,14 @@
 # note : paths can't be defined in quotes??? eg DIR1="~/lab/redesign" not found
 
 SESH="lab"
-DIR1=~/lab/redesign
-DIR2=~/lab/redesign/src
+DIR1=~/lab
+DIR2=~/lab/src
 
 # new session for basic development of lab
 byobu new-session -d -s $SESH -c $DIR1
 
 # basic dev windows
-for i in {1..4}; do byobu new-window -t $SESH -n "" -c $DIR2; done
+for i in {1..4}; do byobu new-window -t $SESH -n "src$i" -c $DIR2; done
 
 # server windows
 for i in {6..7}; do byobu new-window -t $SESH -n "psmn" -c $DIR1; done
--- .byobu_thesis.sh
+++ .byobu_thesis.sh
@@ -13,3 +13,7 @@ byobu new-session -d -s $SESH -c $DIR1
 
 # basic dev windows
 for i in {1..6}; do byobu new-window -t $SESH -n "Ch${i}" -c $DIR2; done
+
+for i in A B C; do byobu new-window -t $SESH -n "app${i}" -c $DIR2; done
+
+for i in {1..3}; do byobu new-window -t $SESH -n "misc${i}" -c $DIR1; done
